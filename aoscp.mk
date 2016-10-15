4ecd2a36747cc3a439ec1ff183b8174bac948207
Chris <chriscrawf95@gmail.com>
2016-10-15 16:14:48 -0700

    AOSCP bringup

---
+++ aoscp.mk
@@ -0,0 +1,32 @@
+# Inherit some common AOSCP stuff.
+$(call inherit-product, vendor/aoscp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aoscp/configs/telephony.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aoscp/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aoscp_angler
+PRODUCT_DEVICE := angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+TARGET_VENDOR := huawei
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NBD90X/3254009:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NBD90X 3254009 release-keys"
